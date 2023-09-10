f3896d81051b00d270859c061e4467984fa747ac
usman <usman12k2@gmail.com>
2023-09-10 14:21:28 +0400

    Updated UI

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
-document.getElementById("listUrls").addEventListener("click", function () {
+// Prevents the code from breaking if the class names change
+const UI = {
+  group_tabs_btn: document.querySelector(".group-tabs-btn")
+}
+
+UI.group_tabs_btn.addEventListener("click", function () {
   chrome.runtime.sendMessage({ action: "listUrls" }, function (response) {
     if (chrome.runtime.lastError) {
       console.error("Error:", chrome.runtime.lastError);
