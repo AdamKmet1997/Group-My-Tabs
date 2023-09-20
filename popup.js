431ac008f5f9aa4278094164fd09f729f9f426ba
usman <usman12k2@gmail.com>
2023-09-20 11:08:49 +0400

    Created UI

--- popup.js
+++ popup.js
@@ -1,12 +1,24 @@
 const UI = {
   group_tabs_btn: document.querySelector("#listUrls"),
+  settingsPageBtn: document.querySelector(".settings-page-btn"),
+  settingsPageContainer: document.querySelector(".settings-container"),
+  mainContainer: document.querySelector(".main-container")
 };
 
-UI.group_tabs_btn.addEventListener("click", function () {
-  chrome.runtime.sendMessage({ action: "listUrls" }, function (response) {
-    if (chrome.runtime.lastError) {
-      console.error("Error:", chrome.runtime.lastError);
-      return;
-    }
+AddEventListeners()
+
+function AddEventListeners(){
+  UI.group_tabs_btn.addEventListener("click", function () {
+    chrome.runtime.sendMessage({ action: "listUrls" }, function (response) {
+      if (chrome.runtime.lastError) {
+        console.error("Error:", chrome.runtime.lastError);
+        return;
+      }
+    });
   });
-});
+  
+  UI.settingsPageBtn.addEventListener("click", () => {
+    UI.settingsPageContainer.classList.toggle("hide")
+    UI.mainContainer.classList.toggle("hide")
+  })
+}
